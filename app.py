de4d2edbbe1a99709ac8b08719cb0cab52b53169
Peppian <peppian9@gmail.com>
2025-07-05 23:59:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import os
 from generative_ai_response import ask_openrouter
 from prompt import generate_price_explanation_prompt
 
-# Konfigurasi
 MODEL_PATH = "data/xgb_price_predictor.joblib"
 COLUMNS_PATH = "data/xgb_model_columns.joblib"
 DATA_PATH =  "data/mobil123_data_updated.csv"
@@ -19,14 +18,12 @@ data = pd.read_csv(DATA_PATH)
 st.set_page_config(page_title="Estimasi Harga Mobil Bekas", page_icon="🚘")
 st.title("🚘 Estimasi Harga Mobil Bekas")
 
-# INPUT UI
 brand_list = sorted(data['name'].dropna().unique())
 brand = st.selectbox("Pilih Merek Mobil", ["-"] + brand_list)
 
 filtered_models = data[data['name'] == brand]['model'].unique() if brand != "-" else []
 model_selected = st.selectbox("Pilih Tipe / Model", ["-"] + list(filtered_models))
 
-# Auto metadata
 row = data[(data['name'] == brand) & (data['model'] == model_selected)].head(1)
 if not row.empty:
     body_type = row['bodyType'].values[0]
@@ -37,7 +34,6 @@ else:
     seating_capacity = "-"
     fuel_type = "-"
 
-# Auto display
 st.markdown(f"**Body Type (auto):** {body_type}")
 st.markdown(f"**Jumlah Kursi (auto):** {seating_capacity if seating_capacity != '-' else '-'} kursi")
 
@@ -46,12 +42,10 @@ transmission = st.selectbox("Transmisi", ["-", "AT", "MT"])
 year = st.slider("Tahun Produksi", 2000, 2025, 2020)
 mileage = st.number_input("Kilometer", min_value=0, value=0)
 
-# PREDIKSI HARGA
 if st.button("🔍 Estimasi Harga"):
     if "-" in [brand, model_selected, region, transmission, fuel_type, body_type, seating_capacity]:
         st.warning("Mohon lengkapi semua input terlebih dahulu.")
     else:
-        # Inisialisasi data input
         input_data = {
             "value_mileage": mileage,
             "vehicleModelDate": year,
@@ -59,15 +53,12 @@ if st.button("🔍 Estimasi Harga"):
             "seatingCapacity": seating_capacity,
         }
 
-        # One-hot encoding manual
         input_encoded = pd.DataFrame([input_data])
 
-        # Kolom dummy
         for col in columns:
             if col not in input_encoded.columns:
-                input_encoded[col] = 0  # default semua kolom kategori = 0
+                input_encoded[col] = 0
 
-        # Set kolom
         feature_flags = {
             f"name_{brand}": 1,
             f"model_{model_selected}": 1,
@@ -80,14 +71,11 @@ if st.button("🔍 Estimasi Harga"):
             if col_name in input_encoded.columns:
                 input_encoded[col_name] = val
 
-        # Urutkan kolom
         input_encoded = input_encoded[columns]
 
-        # Prediksi
         estimated_price = model.predict(input_encoded)[0]
         st.success(f"💰 Estimasi Harga: **Rp {estimated_price:,.0f}**")
 
-        # GENERATIVE AI
         with st.spinner("🧠 Menyusun penjelasan dari AI..."):
             prompt = generate_price_explanation_prompt(
                 brand=brand,
